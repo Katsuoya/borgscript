b9b6114bc7607a25dc6f71cf7820738896137452
Katsuoya <noemail>
2019-01-02 17:51:34 +0100

    changes

--- borgrun.sh
+++ borgrun.sh
@@ -9,17 +9,17 @@ LOG="./backup.log"
 exec > >(tee -i ${LOG})
 exec 2>&1
 
-[[ ! $HASERROR ]] && { bash ./borgupdate.sh; RESULT=$?; [[ ${RESULT} != 0 ]] && HASERROR=1; }
-[[ ! $HASERROR ]] && { bash ./borgbackup.sh; RESULT=$?; [[ ${RESULT} != 0 ]] && HASERROR=1; }
+[[ $HASERROR = 0 ]] && { ./borgupdate.sh; RESULT=$?; [[ ${RESULT} != 0 ]] && HASERROR=1; }
+[[ $HASERROR = 0 ]] && { ./borgbackup.sh; RESULT=$?; [[ ${RESULT} != 0 ]] && HASERROR=1; }
 
 ## Log for Httpsrequest
 if [ -d "/var/www/html/" ]; then
   WSLOG="/var/www/html/backupstatus.log"
   LOGDATE=`date +%Y-%m-%d`
   LOGTIME=`date +%H-%M-%S`
-  [[ $HASERROR ]] && LOGSTATE="error" || LOGSTATE="success"
+  [[ $HASERROR = 1 ]] && LOGSTATE="error" || LOGSTATE="success"
   echo "{\"backup\":[{\"date\":\"${LOGDATE}\",\"time\":\"${LOGTIME}\",\"state\":\"${LOGSTATE}\"}]}" &> $WSLOG
 fi
 
 ## Mail
-[[ ! $HASERROR ]] && mailx -a "From: "${HOSTSHORT^^}" Backup <"$MAILSENDER">" -s "Backup | Success | "${HOSTSHORT^^} $MAILRECEIVER < $LOG
+[[ $HASERROR = 0 ]] && mailx -a "From: "${HOSTSHORT^^}" Backup <"$MAILSENDER">" -s "Backup | Success | "${HOSTSHORT^^} $MAILRECEIVER < $LOG
