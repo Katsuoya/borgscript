92d17d0cdd66906be87de4af2813cf49788c4b43
Katsuoya <noemail>
2019-01-05 23:03:58 +0100

    changes

---
+++ borgnotify.sh
@@ -0,0 +1,19 @@
+
+
+HOSTSHORT=$(hostname -s)
+MAILSENDER=$HOSTSHORT"@"$(cat "$DIR/.borg-domainname")
+MAILRECEIVER=$(cat "$DIR/.borg-mailrecipient")
+HASERROR=$1
+LOG=$2
+
+## Log for Httpsrequest
+if [ -d "/var/www/html/" ]; then
+  WSLOG="/var/www/html/backupstatus.log"
+  LOGDATE=$(date +%Y-%m-%d)
+  LOGTIME=$(date +%H-%M-%S)
+  [ $HASERROR = 1 ] && LOGSTATE="error" || LOGSTATE="success"
+  echo "{\"backup\":{\"date\":\"${LOGDATE}\",\"time\":\"${LOGTIME}\",\"state\":\"${LOGSTATE}\"}}" > $WSLOG
+fi
+
+## Mail
+[ $HASERROR = 0 ] && mailx -a "From: ${HOSTSHORT} Backup <$MAILSENDER>" -s "Backup | Success | ${HOSTSHORT}" $MAILRECEIVER < "$LOG"
--- borgrun.sh
+++ borgrun.sh
@@ -3,25 +3,12 @@
 startborg()
 {
   VERSION=$1
-  HOSTSHORT=$(hostname -s)
-  MAILSENDER=$HOSTSHORT"@"$(cat "$DIR/.borg-domainname")
-  MAILRECEIVER=$(cat "$DIR/.borg-mailrecipient")
   HASERROR=0
 
   [ $HASERROR = 0 ] && { "$DIR/borgupdate.sh" "$VERSION"; RESULT=$?; [ ${RESULT} != 0 ] && HASERROR=1; }
   [ $HASERROR = 0 ] && { "$DIR/borgbackup.sh"; RESULT=$?; [ ${RESULT} != 0 ] && HASERROR=1; }
-
-  ## Log for Httpsrequest
-  if [ -d "/var/www/html/" ]; then
-    WSLOG="/var/www/html/backupstatus.log"
-    LOGDATE=$(date +%Y-%m-%d)
-    LOGTIME=$(date +%H-%M-%S)
-    [ $HASERROR = 1 ] && LOGSTATE="error" || LOGSTATE="success"
-    echo "{\"backup\":[{\"date\":\"${LOGDATE}\",\"time\":\"${LOGTIME}\",\"state\":\"${LOGSTATE}\"}]}" > $WSLOG
-  fi
-
-  ## Mail
-  [ $HASERROR = 0 ] && mailx -a "From: ${HOSTSHORT} Backup <$MAILSENDER>" -s "Backup | Success | ${HOSTSHORT}" $MAILRECEIVER < "$LOG"
+  [ $HASERROR = 0 ] && { "$DIR/borgnotify.sh" "$HASERROR" "$LOG"; RESULT=$?; [ ${RESULT} != 0 ] && HASERROR=1; }
+  [ $HASERROR = 1 ] && { echo "Error occured"; exit 1; }
 }
 
 ####
